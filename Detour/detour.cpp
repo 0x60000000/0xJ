5e13db5bdfa2489df57319d5b046f40e52556be6
Jay <124115662+0x60000000@users.noreply.github.com>
2025-12-30 17:58:45 -0500

    Create detour.cpp

---
+++ Detour/detour.cpp
@@ -0,0 +1,6 @@
+/*Precompiled Header File*/
+#include "pch.h"
+
+#pragma section(".text")
+__declspec(allocate(".text")) BYTE Detour::TrampolineBuffer[200 * 20] = {};
+SIZE_T Detour::TrampolineSize = 0;
