0392a06d255651b64e1594705ca23123b303761c
Jay <124115662+0x60000000@users.noreply.github.com>
2026-01-05 19:27:13 -0500

    Add detours for CL_ConsolePrint and G_ModelIndex

---
+++ Game/detours.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+namespace Detours
+{
+	namespace IW3
+	{
+		namespace Nightly
+		{
+			Detour CL_ConsolePrint_Detour;
+			Detour G_ModelIndex_Detour;
+			Detour SV_ClipMoveToEntity_Detour;
+		}
+	}
+}
