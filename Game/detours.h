afb5b79095e78c67fd193601534877dac929c607
Jay <124115662+0x60000000@users.noreply.github.com>
2026-01-05 19:27:48 -0500

    Add detours.h header for Detours namespace

---
+++ Game/detours.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "detour.h"
+
+namespace Detours
+{
+	namespace IW3
+	{
+		namespace Nightly
+		{
+			extern Detour CL_ConsolePrint_Detour;
+			extern Detour G_ModelIndex_Detour;
+			extern Detour SV_ClipMoveToEntity_Detour;
+		}
+	}
+}
