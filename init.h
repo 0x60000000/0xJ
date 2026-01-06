058ab83aa88360d14f3e10539648356279bf18d3
Jay <124115662+0x60000000@users.noreply.github.com>
2026-01-05 19:21:03 -0500

    Add initialization functions for various games

---
+++ init.h
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace Initialize
+{
+	void CoD2();
+
+	void CoD3();
+
+	void CoD4();
+
+	void CoD5();
+
+	void CoD6();
+
+	void CoD7();
+
+	void CoD8();
+
+	void CoD9();
+
+	void CoD10();
+
+	void CoD11();
+
+	void GTASA();
+
+	void GTAIV();
+
+	void GTAV();
+
+	void SR();
+
+	void SR2();
+
+	void SR3();
+
+	void SR4();
+}
