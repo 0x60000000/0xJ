349e9fa0805fea0d43daa3f97348f5e933f2e327
Jay <124115662+0x60000000@users.noreply.github.com>
2026-01-05 19:24:18 -0500

    Add definitions for CL_ConsolePrint and G_ModelIndex

---
+++ Game/defs.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+namespace Defs
+{
+	namespace IW3
+	{
+		namespace Nightly
+		{
+			CL_CONSOLEPRINT		CL_ConsolePrint		= reinterpret_cast<CL_CONSOLEPRINT>(0x822E4D18);
+			G_MODELINDEX		G_ModelIndex		= reinterpret_cast<G_MODELINDEX>(0x8224CD68);
+			SV_CLIPMOVETOENTITY SV_ClipMoveToEntity = reinterpret_cast<SV_CLIPMOVETOENTITY>(0x823552E0);
+		}
+	}
+}
